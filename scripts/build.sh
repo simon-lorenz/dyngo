70339389874c2702a7eb551a217b1ada65ddb572
Simon Lorenz <mail@simon-lorenz.dev>
2023-04-30 11:48:31 +0200

    build: remove windows build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,10 @@
 echo 'Cleaning build directory'
 rm -r build
 
-echo '[1/3] Building linux/amd64...'
+echo '[1/2] Building linux/amd64...'
 GOARCH=amd64 GOOS=linux go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/dyngo-amd64 .
 
-echo '[2/3] Building linux/arm64...'
+echo '[2/2] Building linux/arm64...'
 GOARCH=arm64 GOOS=linux go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/dyngo-arm64 .
-
-echo '[3/3] Building windows/amd64...'
-GOARCH=amd64 GOOS=windows go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/dyngo.exe .
 
 echo 'Done!'
