fa2a92cd91ed6b71bb22a98bb11593c776ddead6
Simon Lorenz <mail@simon-lorenz.dev>
2023-04-30 12:21:54 +0200

    build: create dyngo-aarch64 based on dyngo-amd64

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,13 @@
 echo 'Cleaning build directory'
 rm -r build
 
-echo '[1/2] Building linux/amd64...'
+echo '[1/3] Building linux/amd64...'
 GOARCH=amd64 GOOS=linux go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/dyngo-amd64 .
 
-echo '[2/2] Building linux/arm64...'
+echo '[2/3] Building linux/arm64...'
 GOARCH=arm64 GOOS=linux go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/dyngo-arm64 .
+
+echo '[3/3] Aliasing dyngo-arm64 to dyngo-aarch64...'
+cp ./build/dyngo-arm64 ./build/dyngo-aarch64
 
 echo 'Done!'
