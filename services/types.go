a6883b5d76eee3b242f0cf31c52f0c44dfd04412
Simon Lorenz <mail@simon-lorenz.dev>
2023-06-04 18:32:40 +0200

    fix: only v6 gets updated if v4 and v6 are both true

--- services/types.go
+++ services/types.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"dyngo/config"
-	"dyngo/helpers"
 	"dyngo/helpers/ip"
 	"dyngo/logger"
 	"time"
@@ -75,26 +74,33 @@ func NewBaseServiceFromGeneric(name string, config config.GenericServiceConfigur
 }
 
 func getDomainsFromConfig(domains []config.DomainConfiguration) []*Domain {
-	activeDomains := helpers.Filter(domains, func(domain config.DomainConfiguration) bool {
-		return domain.V4 || domain.V6
-	})
+	var result []*Domain
 
-	return helpers.Map(activeDomains, func(domain config.DomainConfiguration) *Domain {
-		protocol := ip.IPv4
+	for _, domain := range domains {
+		if domain.V4 {
+			result = append(result, &Domain{
+				Name:     domain.Name,
+				Protocol: ip.IPv4,
+				State: DomainState{
+					Current: "",
+					Target:  "",
+				},
+			})
+		}
 
 		if domain.V6 {
-			protocol = ip.IPv6
+			result = append(result, &Domain{
+				Name:     domain.Name,
+				Protocol: ip.IPv6,
+				State: DomainState{
+					Current: "",
+					Target:  "",
+				},
+			})
 		}
+	}
 
-		return &Domain{
-			Name:     domain.Name,
-			Protocol: protocol,
-			State: DomainState{
-				Current: "",
-				Target:  "",
-			},
-		}
-	})
+	return result
 }
 
 func (service *BaseService) GetDomains() []*Domain {
